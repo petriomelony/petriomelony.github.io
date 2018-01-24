74135f01b014a4d12c803c539cffa634b694d6df
Michael Shum <petriomelony@gmail.com>
2018-01-23 21:13:10 -0500

    Update infinite-jekyll.js

--- js/infinite-jekyll.js
+++ js/infinite-jekyll.js
@@ -9,7 +9,7 @@ $(function() {
   // Load the JSON file containing all URLs
   $.getJSON('/all-posts.json', function(data) {
     postURLs = data["posts"];
-	window.alert(postsToLoad);
+	window.alert($(".post-list").children().length);
     
     // If there aren't any more posts available to load than already visible, disable fetching
     if (postURLs.length <= postsToLoad)
