90c85c1b7cd2d3ad03fa4d5b586058b2f4b3e667
petriomelony <petriomelony@gmail.com>
2018-01-31 15:08:15 -0500

    Implemented MFP for portfolio, uploaded resume

---
+++ js/popupmain.js
@@ -0,0 +1,12 @@
+$('.image-link').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+			}
+		},
+	});
